a5de3c0ae5e80a0642d8b0deb0f2f811493fbb6a
Manish <Manish>
2015-12-26 11:38:26 +0530

    intialize arraylist

--- src/com/aap/list/ArryalistDemo.java
+++ src/com/aap/list/ArryalistDemo.java
@@ -1,5 +1,23 @@
 package com.aap.list;
 
+import java.util.ArrayList;
+
 public class ArryalistDemo {
 
+	public static void main(String[] args) {
+		//Create Array list
+		ArrayList list = new ArrayList();
+		//size of array list
+		System.out.println(list.size());
+		//insert element in array list
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("D");
+		list.add("E");
+		//after intializing array list
+		System.out.println("A I S="+list.size());
+		//elemnet in array list
+		System.out.println(list);
+	}
 }
